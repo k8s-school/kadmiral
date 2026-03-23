5918c50238c10333ae08b534e6f11947d3c3cd0a
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2026-03-23 17:46:06 +0100

    Update current feature

--- talos-cc/csi.sh
+++ talos-cc/csi.sh
@@ -1,1 +1,36 @@
-git clone -b v1.15.0 https://github.com/kubernetes/cloud-provider-openstack/
+#!/bin/bash
+
+set -euxo pipefail
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+git clone -b v1.35.0 https://github.com/kubernetes/cloud-provider-openstack/
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+chmod 700 $DIR/get_helm.sh
+$DIR/get_helm.sh
+
+NS="csi-system"
+
+kubectl create namespace "$NS"
+kubectl label namespace "$NS" \
+  pod-security.kubernetes.io/enforce=privileged \
+  --overwrite
+
+kubectl create secret generic cloud-config --from-file=cloud.conf=$DIR/cloud.conf -n "$NS"
+
+
+helm repo add cpo https://kubernetes.github.io/cloud-provider-openstack
+helm repo update
+
+CHART_VERSION="2.35.0"
+helm install cinder-csi cpo/openstack-cinder-csi \
+  --namespace "$NS" \
+  --version "$CHART_VERSION" \
+  --set secret.enabled=true \
+  --set secret.name=cloud-config \
+  --set "csi.plugin.volumes[0].name=cacert" \
+  --set csi.plugin.volumes[0].hostPath.path=/var/lib/cacert
+
+  # Incorrect config, please check
+  https://github.com/kubernetes/cloud-provider-openstack/blob/master/manifests/controller-manager/cloud-config
